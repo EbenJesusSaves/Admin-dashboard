af8597261e85c2ab5236fbc16d7c9246a5a15190
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-04 08:30:34 +0000

    started eCommerce Comp

--- dashboard/src/pages/Ecommerce.jsx
+++ dashboard/src/pages/Ecommerce.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+
+import { useStateContext } from "../context/ContextProvider";
+
 const Ecommerce = () => {
   return (
-    <div>
-      <h1>THIS IS THE ECOMMERCE COMPONENT</h1>
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"></div>
+      </div>
     </div>
   );
 };
